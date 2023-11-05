482485ae7f9b1ab394c06a071a6589c1cd76c4f7
HsuMonA <HsuMonA@users.noreply.github.com>
2023-11-05 12:27:35 +0100

    complete Lucian's Luscious Lasagna

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,17 @@
+using Microsoft.VisualBasic;
+using System;
+
 class Lasagna
 {
     // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven() => 40;
 
     // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int actualMins) => ExpectedMinutesInOven() - actualMins;
 
     // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int numOfLayers) => numOfLayers * 2;
 
     // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int numOfLayers, int numOfMins) => PreparationTimeInMinutes(numOfLayers) + ExpectedMinutesInOven() - RemainingMinutesInOven(numOfMins);
 }
