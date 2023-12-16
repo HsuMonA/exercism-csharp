598bfb8e8df5243d571a801bc362558e71791ffe
HsuMonA <HsuMonA@users.noreply.github.com>
2023-12-16 19:48:24 +0100

    complete Log Levels

--- csharp/log-levels/LogLevels.cs
+++ csharp/log-levels/LogLevels.cs
@@ -4,16 +4,41 @@ static class LogLine
 {
     public static string Message(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
+        return logLine
+        .Replace("[ERROR]:", "")
+        .Replace("[WARNING]:", "")
+        .Replace("[INFO]:", "")
+        .Trim();
     }
 
     public static string LogLevel(string logLine)
     {
+        if (logLine.IndexOf("ERROR") == 1)
+        {
+            int first = logLine.IndexOf("ERROR");
+            int last = first + "ERROR".Length - 1;
+            string str2 = logLine.Substring(first, last).ToLower();
+            return str2;
+        }
+        else if (logLine.IndexOf("WARNING") == 1)
+        {
+            int first = logLine.IndexOf("WARNING");
+            int last = first + "WARNING".Length - 1;
+            string str2 = logLine.Substring(first, last).ToLower();
+            return str2;
+        }
+        else if (logLine.IndexOf("INFO") == 1)
+        {
+            int first = logLine.IndexOf("INFO");
+            int last = first + "INFO".Length - 1;
+            string str2 = logLine.Substring(first, last).ToLower();
+            return str2;
+        }
         throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
     }
 
     public static string Reformat(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
+        return $"{LogLine.Message(logLine)} ({LogLine.LogLevel(logLine)})";
     }
 }
